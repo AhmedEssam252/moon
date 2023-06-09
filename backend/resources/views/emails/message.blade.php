df7d12bc0002fa60488ee453c968cbf88907f3db
Ahmed Essam <76502870+AhmedEssam252@users.noreply.github.com>
2023-06-09 07:17:36 +0300

    commit

--- backend/resources/views/emails/message.blade.php
+++ backend/resources/views/emails/message.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
      <section id="mail">
-        <p style="font-size: 22px;font-weight: 600;text-align: center;margin: 50px;color:#fff;">thanks for submit your we will send to you soon</p>
+        <p style="font-size: 22px;font-weight: 600;text-align: center;margin: 50px;color:#fff;">thanks for submit we will send to you soon</p>
      </section>
 @endsection
 
